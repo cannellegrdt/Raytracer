685ac1323205999cf623e3d3036c71fcecc4eac3
Cannelle Gourdet <cannelle.gourdet@epitech.eu>
2026-05-10 00:14:03 +0200

    add: complete unit tests for libconfigLoader
    Signed-off-by: Cannelle Gourdet <cannelle.gourdet@epitech.eu>

--- tests/unit_tests/plugins/test_libconfigloader.cpp
+++ tests/unit_tests/plugins/test_libconfigloader.cpp
@@ -94,3 +94,293 @@ Test(libconfigloader, load_with_group_translation_succeeds) {
 
     cr_assert_no_throw(loader.load(fixturePath("valid_group_translation.cfg"), factory));
 }
+
+Test(libconfigloader, load_textured_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_textured_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_chessboard_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_chessboard_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_marble_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_marble_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_normalmap_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_normalmap_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_transparency_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_transparency_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_phong_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_phong_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_refraction_material_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_refraction_material.cfg"), factory));
+}
+
+Test(libconfigloader, load_invalid_ior_throws_runtime_error) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_throw(loader.load(fixturePath("invalid_ior.cfg"), factory), std::runtime_error);
+}
+
+Test(libconfigloader, load_torus_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("torus", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_torus.cfg"), factory));
+}
+
+Test(libconfigloader, load_cube_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("cube", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_cube.cfg"), factory));
+}
+
+Test(libconfigloader, load_tanglecube_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("tanglecube", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_tanglecube.cfg"), factory));
+}
+
+Test(libconfigloader, load_triangle_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("triangle", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_triangle.cfg"), factory));
+}
+
+Test(libconfigloader, load_mandelbulb_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("mandelbulb", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_mandelbulb.cfg"), factory));
+}
+
+Test(libconfigloader, load_mobius_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("mobius", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_mobius.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_scale_transform_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_scale_transform.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_shear_transform_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_shear_transform.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_rotation_transform_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_rotation_transform.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_translation_transform_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_translation_transform.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_matrix_transform_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_matrix_transform.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_uniform_antialiasing_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_uniform_antialiasing.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_adaptive_antialiasing_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_adaptive_antialiasing.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_nbAORays_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_renderer_aorays.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_point_light_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_point_light.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_ambient_maxdist_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_ambient_maxdist.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_background_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_background.cfg"), factory));
+}
+
+Test(libconfigloader, load_with_imports_succeeds) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_no_throw(loader.load(fixturePath("valid_imports.cfg"), factory));
+}
+
+Test(libconfigloader, load_circular_import_throws_runtime_error) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_throw(loader.load(fixturePath("circular_a.cfg"), factory), std::runtime_error);
+}
+
+Test(libconfigloader, load_parse_error_throws_runtime_error) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_throw(loader.load(fixturePath("parse_error.cfg"), factory), std::runtime_error);
+}
+
+Test(libconfigloader, load_missing_camera_throws_runtime_error) {
+    LibconfigLoader loader;
+    PrimitiveFactory factory;
+    factory.registerType("sphere", []() {
+        return PrimitivePtr(new MockPrimForLoader(), [](IPrimitive *p) { delete p; });
+    });
+
+    cr_assert_throw(loader.load(fixturePath("missing_camera.cfg"), factory), std::runtime_error);
+}
