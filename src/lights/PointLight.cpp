cb68c00af0f24b90f1ad10a2d4671fb756f3f0f6
Cannelle Gourdet <cannelle.gourdet@epitech.eu>
2026-05-11 12:08:08 +0200

    add: guard for div by 0 in PointLight
    Signed-off-by: Cannelle Gourdet <cannelle.gourdet@epitech.eu>

--- src/lights/PointLight.cpp
+++ src/lights/PointLight.cpp
@@ -11,6 +11,14 @@ LightSample PointLight::getSample(const Vec3 &hitPoint, const Vec3 &/*normal*/) 
     Vec3 dir = _position - hitPoint;
     double lenDir = length(dir);
 
+    if (lenDir < 1e-8) {
+        return LightSample{
+            Vec3(0, 0, 0),
+            Vec3(0, 0, 0),
+            lenDir
+        };
+    }
+
     return LightSample{
         normalize(dir),
         _color * _intensity / (lenDir * lenDir),
